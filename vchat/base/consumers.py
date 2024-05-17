f87714ddd29e5de25efd0c18a92a4c9583598084
Vivekroy286 <286royv@gmail.com>
2024-05-18 01:31:49 +0530

    taking username as input

--- vchat/base/consumers.py
+++ vchat/base/consumers.py
@@ -1,17 +1,40 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "Test-Room"
+        
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name
+        )
+        
+        print('Disconnected!')
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    async def receive(self, text_data):
+        recieve_dict = json.loads(text_data)
+        message = recieve_dict["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": 'send.message',
+                'message' : message
+                }
+            )
+        async def send_message(self, event):
+            message = event['message']
+            
+            await self.send(text_data=json.dumps({
+                'message': message
+            }))
+            
--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_WSGI_APPLICATION = 'vchat.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}

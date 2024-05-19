0cbba6e2c2e6d08f8d4ff7ee9684bbe70e037e08
Vivekroy286 <286royv@gmail.com>
2024-05-19 17:51:35 +0530

    web socket handshaking and connect

--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ASGI_WSGI_APPLICATION = 'vchat.asgi.application'
+ASGI_APPLICATION = 'vchat.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {

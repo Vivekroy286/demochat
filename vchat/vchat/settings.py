9d930c912214594f248176c4b2b23dac548e5601
Vivekroy286 <286royv@gmail.com>
2024-05-21 23:58:32 +0530

    adding ui styling lib

--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -40,7 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'channels',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    
+    "127.0.0.1",
+    
+]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'vchat.urls'

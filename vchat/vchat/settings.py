e890253f714d969f52b0ce3a4bfff91ba43c71fd
Vivekroy286 <286royv@gmail.com>
2024-05-30 13:02:26 +0530

    changing gunicon to waitress

--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'vchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}"""
 
 
 

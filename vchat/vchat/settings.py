20240f10b46a3092ab21bf05abb9b10acee21eec
Vivekroy286 <286royv@gmail.com>
2024-06-05 02:59:28 +0530

    chnging to asgi server

--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 #import environ
 
 
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = 'static/'
--- vchat/vchat/urls.py
+++ vchat/vchat/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += staticfiles_urlpatterns()

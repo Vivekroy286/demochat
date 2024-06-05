27146dc0db9f6f8499e5f15ab3a89ae587f0476a
Vivekroy286 <286royv@gmail.com>
2024-06-05 08:15:10 +0530

    STATIC_URL = '/static/'

--- vchat/vchat/settings.py
+++ vchat/vchat/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 

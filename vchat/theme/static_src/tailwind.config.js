f7a5569afab90ac7c7a6b2de3e7a95457f608b6d
Vivekroy286 <286royv@gmail.com>
2024-06-08 21:06:33 +0530

    testing ui login room

--- vchat/theme/static_src/tailwind.config.js
+++ vchat/theme/static_src/tailwind.config.js
@@ -42,7 +42,16 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'black-dark': '#00000050',
+                'dull-white': '#FFFFFFB3',
+                'white-light': '#FFFFFF30',
+                'white-medium': '#FFFFFF40',
+                'neon-blue': '#2FB8FF',
+
+            }
+        },
     },
     plugins: [
         /**

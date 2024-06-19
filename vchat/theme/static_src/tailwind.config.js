c4aa79a09ab0146048a29d3e2c9e928f003f51d6
Vivekroy286 <286royv@gmail.com>
2024-06-19 11:20:37 +0530

    update19

--- vchat/theme/static_src/tailwind.config.js
+++ vchat/theme/static_src/tailwind.config.js
@@ -42,11 +42,12 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-
-        extend: {
-            colors: {
-
-            }
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            // Add more breakpoints as needed
         },
     },
     plugins: [
